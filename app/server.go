14a5ac46433dea89e64a19e33d3b0a9815cf91dd
sean333 <seanbrandenburg333@gmail.com>
2024-06-16 10:31:15 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,23 +2,42 @@ package main
 
 import (
 	"fmt"
-	
+
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port 6379", err.Error())
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	clientConn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer clientConn.Close()
+
+	data := make([]byte, 128)
+	bytesRead, err := clientConn.Read(data)
+	if err != nil {
+		fmt.Println("Error reading from client connection: ", err.Error())
+		os.Exit(1)
+	}
+	
+	fmt.Printf("Read %d bytes: \n", bytesRead)
+	fmt.Println("Received message: ", string(data[:bytesRead]))
+
+	respString := "+PONG\r\n"
+	// for range 2 {
+		fmt.Println("Writing message: ", respString)
+		_, err = clientConn.Write([]byte(respString))
+		if err != nil {
+			fmt.Println("Error writing response to client", err.Error())
+			os.Exit(1)
+		}
+	// }
 }
